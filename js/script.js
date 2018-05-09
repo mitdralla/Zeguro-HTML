5a40b2637518be076079079e34a2abc18647cbf5
Timothy Allard <timallard@gmail.com>
2018-05-09 10:13:31 -0500

    map link, coverages umbrella

--- js/script.js
+++ js/script.js
@@ -70,6 +70,18 @@ $(document).ready(function() {
     ]
   });
 
+    $(function() {
+        $(".video").click(function () {
+            var theModal = $(this).data("target"),
+                videoSRC = $(this).attr("data-video"),
+                videoSRCauto = videoSRC + "?modestbranding=1&rel=0&controls=1&showinfo=0&html5=1&autoplay=1width=560&height=315";
+            $(theModal + ' iframe').attr('src', videoSRCauto);
+            $(theModal).on('click', function(){
+                $(theModal + ' iframe').attr('src', videoSRC);
+            });
+        });
+    });
+
 });
 
 
